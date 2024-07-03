61b4cd16eecefc42d5b018440847cab31f2b526e
l.kirchhart <l.kirchhart@ANB1DE060.corpdir.zz>
2024-07-03 16:39:44 +0200

    ein paar Dateien vergessen

---
+++ myfirstappLKI/src/test/java/com/in28minutes/springboot/myfirstappLKI/MyfirstappLkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.myfirstappLKI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstappLkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
