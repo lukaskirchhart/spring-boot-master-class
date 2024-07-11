49a13a28e2b24717b3e7a15f885b1620b561081e
l.kirchhart <l.kirchhart@ANB1DE060.corpdir.zz>
2024-07-11 10:11:08 +0200

    add authentification service and redirect login or welcome

--- myfirstappLKI/src/main/java/com/in28minutes/springboot/myfirstappLKI/login/LoginController.java
+++ myfirstappLKI/src/main/java/com/in28minutes/springboot/myfirstappLKI/login/LoginController.java
@@ -1,24 +1,47 @@
 package com.in28minutes.springboot.myfirstappLKI.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	
-	private Logger logger = LoggerFactory.getLogger(getClass());
+	private AuthentificationService authentification;
 	
-	@RequestMapping("login")
-	public String login(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
-		logger.debug("Request param is {}",name);
+	public LoginController(AuthentificationService authentification) {
+		this.authentification=authentification;
+	}
+
+	//GET, POST
+	@GetMapping("login")
+	public String loginGet() {
+//		model.put("name", name);
+//		logger.debug("Request param is {}",name);
+		
+		
 		//open META-INF/resources/WEB-INF/jsp/login.jsp
 		//Open *.jsp is defined in application.properties
 		return "login";
+	}
+	
+	@PostMapping("login")
+	public String gotoWelcomePage(@RequestParam String name,  @RequestParam String password, ModelMap model) {
+		model.put("name", name);
+		model.put("password", password);
+		
+		
+		authentification = new AuthentificationService();
+		if(authentification.authenticate(name, password)) {			
+			return "welcome";
+		}else {
+			model.put("loginFailed", "Invalid credentials. Please try again");
+			return "login";
+		}
+		
 	}
 
 }
--- myfirstappLKI/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ myfirstappLKI/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,7 +3,12 @@
 		<title>Login -JSP</title>
 	</head>
 	<body>
-		login - JSP<br>
-		Hello ${name}
+		Hello to Login Page
+		<pre>${loginFailed}</pre>
+		<form method="post">
+			Name: <input type="text" name="name">
+			Password: <input type="password" name="password">
+			<input type="submit">
+		</form>
 	</body>
 </html>
