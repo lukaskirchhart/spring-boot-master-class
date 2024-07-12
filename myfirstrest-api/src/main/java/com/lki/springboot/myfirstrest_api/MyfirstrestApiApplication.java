8f514dac2f14dbca036769db620d71456165385b
l.kirchhart <l.kirchhart@ANB1DE060.corpdir.zz>
2024-07-12 16:50:01 +0200

    add missing files

---
+++ myfirstrest-api/src/main/java/com/lki/springboot/myfirstrest_api/MyfirstrestApiApplication.java
@@ -0,0 +1,13 @@
+package com.lki.springboot.myfirstrest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstrestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstrestApiApplication.class, args);
+	}
+
+}
---
+++ myfirstrest-api/src/test/java/com/lki/springboot/myfirstrest_api/MyfirstrestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lki.springboot.myfirstrest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstrestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
